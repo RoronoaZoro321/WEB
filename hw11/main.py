0e0814a1bebfe7495b0b84f4a90b2b80adf7ba76
Zoro <145021198+RoronoaZoro321@users.noreply.github.com>
2023-10-06 23:18:14 +0700

    not done

---
+++ hw11/main.py
@@ -0,0 +1,82 @@
+import ZODB, ZODB.FileStorage
+import persistent
+import transaction
+from z_enrollment import Course, Student, Enrollment
+
+storage = ZODB.FileStorage.FileStorage('mydata.fs')
+db = ZODB.DB(storage)
+connection = db.open()
+root = connection.root
+
+import BTrees.OOBTree
+root.courses = BTrees.OOBTree.BTree()
+root.students = BTrees.OOBTree.BTree()
+root.enrollments = BTrees.OOBTree.BTree()
+
+def addCourse():
+    root.courses['101'] = Course('101', 'Computer Programming', 4)
+    root.courses['201'] = Course('201', 'Web Programming', 4)
+    root.courses['202'] = Course('202', 'Software Engineering Principle', 5)
+    root.courses['301'] = Course('301', 'Artificail Intelligent', 3)
+
+def addStudent_1():
+    root.students['1100'] = Student('1100', 'Mr. Name ForExample')
+    enroll1 = root.students['1100'].enrollCourse(root.courses['101'])
+    enroll1.setGrade("B")
+    enroll2 = root.students['1100'].enrollCourse(root.courses['201'])
+    enroll2.setGrade("B")
+    enroll3 = root.students['1100'].enrollCourse(root.courses['202'])
+    enroll3.setGrade("C")
+    enroll4 = root.students['1100'].enrollCourse(root.courses['301'])
+    enroll4.setGrade("C")
+
+# def addStudent_2():
+#     root.students['1102'] = Student('1102', 'Mr. Zhong Li')
+#     enroll1 = root.students['1102'].enrollCourse(root.courses['101'])
+#     enroll1.setGrade("A")
+#     enroll2 = root.students['1102'].enrollCourse(root.courses['201'])
+#     enroll2.setGrade("B")
+#     enroll3 = root.students['1102'].enrollCourse(root.courses['202'])
+#     enroll3.setGrade("D")
+
+# def addStudent_3():
+#     root.students['1103'] = Student('1103', 'Mr. Dvalinn Durinson')
+#     enroll1 = root.students['1103'].enrollCourse(root.courses['101'])
+#     enroll1.setGrade("C")
+#     enroll2 = root.students['1103'].enrollCourse(root.courses['201'])
+#     enroll2.setGrade("A")
+#     enroll3 = root.students['1103'].enrollCourse(root.courses['202'])
+#     enroll3.setGrade("B")
+#     enroll4 = root.students['1103'].enrollCourse(root.courses['301'])
+#     enroll4.setGrade("C")
+
+
+# def retrive_course(id):
+#     return root.courses[id]
+
+
+# c = root.courses['101']
+# c.setName('Computer Programming2')
+# transaction.commit()
+# print(retrive_course('101'))
+
+
+
+if __name__ == "__main__":
+    
+    addCourse()
+    addStudent_1()
+    # addStudent_2()
+    # addStudent_3()
+
+    courses = root.courses
+    for c in courses:
+        course = courses[c]
+        course.printDetail()
+    print()
+
+    students = root.students
+    for s in students:
+        student = students[s]
+        student.printTranscript()
+    print()
---
+++ hw11/z_enrollment.py
@@ -0,0 +1,107 @@
+import persistent
+
+class Course(persistent.Persistent):
+    def __init__(self, id, name="", credit=0):
+        self.id = id
+        self.name = name
+        self.credit = credit
+        self.gradeScheme = [
+            {"Grade": "A", "Min": 80, "Max": 100},
+            {"Grade": "B", "Min": 70, "Max": 79},
+            {"Grade": "C", "Min": 60, "Max": 69},
+            {"Grade": "D", "Min": 50, "Max": 59},
+            {"Grade": "F", "Min": 0, "Max": 49}
+        ]
+    def __str__(self):
+        return f"ID: \t{self.id}  Course: \t{self.name:<30}  ,  Credit: {self.credit}"
+    def setName(self, name):
+        self.name = name
+    def printDetail(self):
+        print(self.__str__())
+    def scoreGrading(self, score):
+        for grade in self.gradeScheme:
+            if score >= grade["Min"] and score <= grade["Max"]:
+                return grade["Grade"]
+    def setGradeScheme(self, scheme):
+        self.gradeScheme = scheme
+
+class Student(persistent.Persistent):
+    def __init__(self, id, name=""):
+        self.enroll = []
+        self.id = id
+        self.name = name
+
+    def enrollCourse(self, course):
+        x = Enrollment(self, course)
+        self.enroll.append(x)
+        return x
+        
+    def getEnrollment(self, course):
+        for enroll in self.enroll:
+            if enroll.getCourse() == course:
+                return enroll
+        return None
+    
+    def getGPA(self):
+        totalCredit = 0
+        totalGrade = 0
+        for enroll in self.enroll:
+            course = enroll.getCourse()
+            credit = course.credit
+            grade = enroll.getGrade()
+            if grade == "A":
+                totalGrade += 4 * credit
+            elif grade == "B":
+                totalGrade += 3 * credit
+            elif grade == "C":
+                totalGrade += 2 * credit
+            elif grade == "D":
+                totalGrade += 1 * credit
+            elif grade == "F":
+                totalGrade += 0 * credit
+            totalCredit += credit
+        return format(totalGrade / totalCredit, ".2f") 
+
+    def printTranscript(self):
+        print("\t Transcript")
+        print(f"ID: \t {self.id}  Name: {self.name}")
+        print("Course list")
+        for enroll in self.enroll:
+            course = enroll.getCourse()
+            grade = enroll.getGrade()
+            print(
+                f"    ID: {course.id:>10}  Course: {course.name:<30} , Credit {course.credit:>5} Grade: {grade:>5}")
+        print("Total GPA is: ", self.getGPA())
+        print()
+
+    def setName(self, name):
+        self.name = name
+
+class Enrollment(persistent.Persistent):
+    def __init__(self, student, course, score):
+        self.student = student
+        self.course = course
+        self.score = score
+    def __str__(self):
+        return f"Enrollment({self.student}, {self.course}, {self.score})"
+    def getCourse(self):
+        return self.course
+    def printDetail(self):
+        print(self.__str__())
+    def getScore(self):
+        return self.score
+    def setScore(self, score):
+        self.score = score
+    def getGrade(self):
+        s = self.score
+        if s >= 80 and s <= 100:
+            return "A"
+        elif s >= 70 and s <= 79:
+            return "B"
+        elif s >= 60 and s <= 69:
+            return "C"
+        elif s >= 50 and s <= 59:
+            return "D"
+        else:
+            return "F"
+
